71d938b9d84183c2c5cc88b32ee0db9a9b16c3d0
TheoBessel <theo.bessel@outlook.com>
2025-05-06 11:16:35 +0200

    feat: Add a bit of styling to device cards

--- app/components/ui/deviceCard.tsx
+++ app/components/ui/deviceCard.tsx
@@ -1,4 +1,4 @@
-import { WifiHighIcon, WifiIcon, WifiOffIcon } from "lucide-react";
+import { ServerCrashIcon, ServerIcon } from "lucide-react";
 
 type DeviceCardProps = {
     name: string;
@@ -11,31 +11,26 @@ type DeviceCardProps = {
 
 export default function DeviceCard({name, cpu, ram, room, status}: DeviceCardProps) {
     return (
-        <div key={name} className="flex items-center gap-4 justify-stretch bg-zinc-100 p-2 w-full h-28 rounded-xl">
-            <div>
-                <p className="text-3xl">{name}</p>
+        <div key={name} className="flex items-center gap-4 bg-zinc-100 p-4 w-full h-28 rounded-xl justify-between">
+            <div className="flex flex-col">
+                <p className="text-2xl">{name}</p>
                 <p className="text-base text-stone-400">{cpu}</p>
                 <p className="text-base text-stone-400">{ram && ram + "go RAM"}</p>
             </div>
-            <>
-            {status === "ONLINE" ? (
-            <div className="flex items-center gap-2 justify-start w-32">
-                <WifiIcon className="size-5 stroke-[3px] stroke-green-500"/>
-                <p className="text-xl text-green-500">Up</p>
-            </div>
-            ) : status === "OFFLINE" ? (
-            <div className="flex items-center gap-2 justify-start w-32">
-                <WifiOffIcon className="size-5 stroke-[3px] stroke-red-500"/>
-                <p className="text-xl text-red-500">Down</p>
-            </div>) :
-            <div className="flex items-center gap-2 justify-start w-32">
-                <WifiHighIcon className="size-5 stroke-[3px] stroke-yellow-500"/>
-                <p className="text-xl text-yellow-500">Pending</p>
-            </div>
-            }
-            </>
-            <div className="flex items-center justify-between w-10">
-            <p className="text-xl">{room}</p>
+            <div className="flex flex-col items-end gap-2">
+                {status === "ONLINE" ? (
+                    <div className="flex items-center gap-2 justify-start bg-blue-400 rounded-full p-2 size-10">
+                        <ServerIcon className="size-8 stroke-[2px] stroke-white"/>
+                    </div>
+                    ) : status === "OFFLINE" ? (
+                    <div className="flex items-center gap-2 justify-start bg-gray-400 rounded-full p-2 size-10">
+                        <ServerCrashIcon className="size-8 stroke-[3px]"/>
+                    </div>) :
+                    <div className="flex items-center gap-2 justify-start bg-gray-400 rounded-full p-2 size-10">
+                        <ServerCrashIcon className="size-8 stroke-[3px]"/>
+                    </div>
+                }
+                <p className="text-base">Dernière actualisation : 2h18</p>
             </div>
         </div>
     );
