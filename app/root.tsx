5aa8044720daa33f01c34746df767294ca9bfa53
TheoBessel <theo.bessel@outlook.com>
2025-05-14 23:05:32 +0200

    feat: Add error page

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,14 @@
 import type { LinksFunction } from "@remix-run/node";
 import {
+  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
+import N7WSlogo from "~/public/N7WS.png";
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
@@ -32,6 +35,43 @@ export default function App() {
       </head>
       <body className="overflow-hidden">
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  let title = "Unexpected Error";
+  let message = "Something went wrong.";
+
+  if (isRouteErrorResponse(error)) {
+    title = `${error.status} - ${error.statusText}`;
+    message = error.data || message;
+  } else if (error instanceof Error) {
+    message = error.message;
+  }
+
+  return (
+    <html lang="en">
+      <head>
+        <title>{title}</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="relative min-h-screen bg-gray-100">
+        {/* Logo en haut à gauche */}
+        <img alt="logo" src={N7WSlogo} className="absolute top-4 left-4 w-28" />
+
+        {/* Contenu centré verticalement et horizontalement */}
+        <div className="flex flex-col items-center justify-center min-h-screen text-center px-8">
+          <h1 className="text-4xl font-bold mb-4 text-blue-400">{title}</h1>
+          <p className="text-lg text-gray-700">{message}</p>
+        </div>
+
         <ScrollRestoration />
         <Scripts />
       </body>
